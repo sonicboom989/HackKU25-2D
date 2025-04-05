6657c2cc092ea1b1f49d7909ca7718681f20df99
Ziv Cohen <zcohen@ku.edu>
2025-04-05 02:46:33 -0500

    Created Initial Character Animation

---
+++ Test/Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    //references
+    Animator am;
+    PlayerMovement pm;
+    SpriteRenderer sr;
+
+
+
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        am = GetComponent<Animator>();
+        pm = GetComponent<PlayerMovement>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pm.moveDir.x != 0 || pm.moveDir.y != 0)
+        {
+            am.SetBool("Move", true);
+        }
+        else
+        {
+            am.SetBool("Move", false);
+        }
+
+    }
+}
