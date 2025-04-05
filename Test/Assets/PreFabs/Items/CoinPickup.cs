198be5abf77ae38d2a2ae51ab7a007ac0c222400
cringey303 <lucas@the-b.org>
2025-04-05 08:48:09 -0500

    coin pickup and counter working

--- Test/Assets/PreFabs/Items/CoinPickup.cs
+++ Test/Assets/PreFabs/Items/CoinPickup.cs
@@ -4,12 +4,14 @@ public class CoinPickup : MonoBehaviour
 {
     public int coinValue = 1;
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
+        Debug.Log("Coin touched by: " + other.gameObject.name);
         if(other.CompareTag("Player"))
         {
             FindObjectOfType<CoinManager>().AddCoins(coinValue);
             Destroy(gameObject);
         }
     }
+
 }
