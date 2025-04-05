a66be6ab076dcd53163994c768a784528020e46c
Ziv Cohen <zcohen@ku.edu>
2025-04-05 06:08:57 -0500

    Beginning to create enemies

---
+++ Test/Assets/PreFabs/Enemies/Scripts/EnemyAI.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public float moveSpeed = 2f;
+    private Transform player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        if (player != null)
+        {
+            Vector2 direction = (player.position - transform.position).normalized;
+            transform.position += (Vector3)direction * moveSpeed * Time.deltaTime;
+        }
+    }
+}
